73eee8a6ccc0eb0cf6d64033da41962406c9602e
criz <96567084+2-3-32@users.noreply.github.com>
2025-04-23 10:30:19 +0000

    2nd commit for routing

--- my-angular-app/src/app/app.routes.ts
+++ my-angular-app/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './routing/home/home.component';
+import { ContactComponent } from './routing/contact/contact.component';
+import { PaymentComponent } from './routing/payment/payment.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' } // Default route
+];
